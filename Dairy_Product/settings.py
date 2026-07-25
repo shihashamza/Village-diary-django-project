cd88114fb9de1efe2095089ecc8a16384c09bb22
shihashamza <shihashamza@gmail.com>
2026-07-25 12:36:18 +0530

    Fix ALLOWED_HOSTS and DEBUG for production

--- Dairy_Product/settings.py
+++ Dairy_Product/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['village-diary-django-project.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
