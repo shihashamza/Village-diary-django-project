ff8929b47f5d9e8d909bc9ce4d4a1581ab704fef
shihashamza <shihashamza@gmail.com>
2026-07-25 12:06:59 +0530

    small change

--- Dairy_Product/settings.py
+++ Dairy_Product/settings.py
@@ -90,8 +90,12 @@ AUTHENTICATION_BACKENDS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydb',
+        'USER': 'dairy_db',
+        'PASSWORD': 'dairy123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
