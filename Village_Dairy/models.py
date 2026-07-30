2d5510bfb0631e1508604cbcbda1b41a20ec9380
shihashamza <shihashamza@gmail.com>
2026-07-30 11:03:14 +0530

    models change

--- Village_Dairy/models.py
+++ Village_Dairy/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
 from django.db import models
 from django.contrib.auth.models import AbstractUser
@@ -16,15 +16,15 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True, default="")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
-    image1 = models.ImageField(upload_to='products/')
-    image2 = models.ImageField(upload_to='products/', blank=True, null=True)
-    image3 = models.ImageField(upload_to='products/', blank=True, null=True)
-    image4 = models.ImageField(upload_to='products/', blank=True, null=True)
+
+    image1 = CloudinaryField('image')
+    image2 = CloudinaryField('image', blank=True, null=True)
+    image3 = CloudinaryField('image', blank=True, null=True)
+    image4 = CloudinaryField('image', blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -37,7 +37,7 @@ class Banner(models.Model):
     )
 
     title = models.CharField(max_length=200)
-    image = models.ImageField(upload_to='banners/')
+    image = CloudinaryField('image')   # ✅ FIXED
     page = models.CharField(max_length=50, choices=PAGE_CHOICES)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -73,11 +73,10 @@ class Address(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone = models.CharField(max_length=15, blank=True)
-    image = models.ImageField(upload_to='profile_pics/', default='default.jpg')
+    image = CloudinaryField('image', default='default')  # ✅ FIXED
     
     def __str__(self):
         return self.user.username
-
 class Order(models.Model):
 
     STATUS_CHOICES = [
